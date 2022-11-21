825caf1202eda80a3699c246ac70ff4d905a49bb
Jakubk12 <jakubkeska11@gmail.com>
2022-11-21 19:21:32 +0100

    small change

--- src/pages/routercomponents/ComponentResources/ProjectsResources.vue
+++ src/pages/routercomponents/ComponentResources/ProjectsResources.vue
@@ -1,10 +1,9 @@
 <template>
       <div class="elements-one">
         <div class="content"><p class="head">{{title}}</p> <p>{{description}}</p></div>
+        <div>{{img}}</div>
         </div>
-        <div class="slide">
 
-</div>
 </template>
 <script>
 export default {
@@ -27,25 +26,13 @@ margin-left: 2vw;
 .img{
   width: 30vw;
   height: 30vh;
-  background-image:("/1proj.png");
   background-size: 100% 100%;
   box-shadow: rgba(150, 160, 170, 0.15) 0px 5px 15px;
-  animation: changeImage 15s infinite;
-  display: block;
+
 
 
 }
-@keyframes changeImage {
-  0% {
-    background-image:("/1proj.png")
-  }
-  50% {
-    background-image:("/2proj.png")
-  }
-  100% {
-    background-image:("/1proj.png")
-  }
-}
+
 }
 </style>
 
